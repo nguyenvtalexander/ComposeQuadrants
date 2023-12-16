d7c0c938fe677ed9a514ac37c22b5fca1f7dbafe
Alexander Nguyen <nguyenvtalexander@gmail.com>
2023-12-16 12:56:43 -0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+<<<<<<< HEAD
+rootProject.name = "ComposeQuadrants"
+=======
+rootProject.name = "ComposeTaskManager"
+>>>>>>> origin/main
+include(":app")
